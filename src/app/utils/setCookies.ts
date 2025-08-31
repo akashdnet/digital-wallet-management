f42e9c9f98f599e8bf093640b019478eee17024b
akashdnet <khondokerakashmi@gmail.com>
2025-08-31 10:42:02 +0600

    cookies code updated

--- src/app/utils/setCookies.ts
+++ src/app/utils/setCookies.ts
@@ -1,23 +1,25 @@
 import { Response } from "express";
+import { envList } from "../config/envList";
 
 export interface AuthTokens {
-    accessToken?: string;
-    refreshToken?: string;
+  accessToken?: string;
+  refreshToken?: string;
 }
 
 export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
+  if (tokenInfo.accessToken) {
+    res.cookie("accessToken", tokenInfo.accessToken, {
+      httpOnly: true,
+      secure: envList.NODE_ENV !== "development",
+      sameSite: "none",
+    });
+  }
 
-    if(tokenInfo.accessToken) {
-        res.cookie("accessToken", tokenInfo.accessToken, {
-            httpOnly: true,
-            secure: false
-        })
-    }
-
-    if(tokenInfo.refreshToken) {
-        res.cookie("refreshToken", tokenInfo.refreshToken, {
-            httpOnly: true,
-            secure: false,
-        })
-    }
-}
+  if (tokenInfo.refreshToken) {
+    res.cookie("refreshToken", tokenInfo.refreshToken, {
+      httpOnly: true,
+      secure: envList.NODE_ENV !== "development",
+      sameSite: "none",
+    });
+  }
+};
